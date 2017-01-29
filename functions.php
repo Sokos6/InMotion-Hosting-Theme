04778c4660b390830e64671ac67a6d6c6f0c72da
Sokos6 <will@comicbooked.com>
2017-01-29 17:06:21 -0500

    portfolio pieces and post types

--- functions.php
+++ functions.php
@@ -1,5 +1,18 @@
 <?php
 
+add_theme_support( 'menus');
+add_theme_support( 'post-thumbnails' );
+
+function register_theme_menus() {
+
+	register_nav_menus(
+		array(
+			'primary-menu' => __( 'Primary Menu' )
+			)
+		);
+}
+add_action( 'init', 'register_theme_menus' );
+
 function imh_theme_styles() {
 
 	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css');
@@ -12,7 +25,7 @@ add_action( 'wp_enqueue_scripts', 'imh_theme_styles' );
 
 function imh_theme_js() {
 	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );
-	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '', true );
+	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
 	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'imh_theme_js');
